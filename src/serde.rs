aef3ad4be34116b4df6ff384b5e7f3c996709bd3
Truc Vy <trucvy05062003@gmail.com>
2025-04-23 17:53:53 +0700

    update serde for paillier key

--- src/serde.rs
+++ src/serde.rs
@@ -1,3 +1,5 @@
+// TODO: update this to use the new API
+
 use rug::Integer;
 
 use crate::{DecryptionKey, EncryptionKey};
@@ -7,7 +9,7 @@ impl serde::Serialize for EncryptionKey {
     where
         S: serde::Serializer,
     {
-        self.n().serialize(serializer)
+        (self.n_size, self.a_size, self.h.clone(), self.n.clone()).serialize(serializer)
     }
 }
 
@@ -16,8 +18,10 @@ impl<'de> serde::Deserialize<'de> for EncryptionKey {
     where
         D: serde::Deserializer<'de>,
     {
-        let n = Integer::deserialize(deserializer)?;
-        Ok(EncryptionKey::from_n(n))
+        let (n_size, a_size, h, n) = <(u32, u32, Integer, Integer)>::deserialize(deserializer)?;
+        Ok(EncryptionKey::new(n_size, a_size, h, n).map_err(|_| {
+            <D::Error as serde::de::Error>::custom("invalid paillier encryption key")
+        })?)
     }
 }
 
@@ -26,8 +30,7 @@ impl serde::Serialize for DecryptionKey {
     where
         S: serde::Serializer,
     {
-        let pq = [self.p(), self.q()];
-        pq.serialize(serializer)
+        [self.n_size(), self.a_size()].serialize(serializer)
     }
 }
 
@@ -36,8 +39,8 @@ impl<'de> serde::Deserialize<'de> for DecryptionKey {
     where
         D: serde::Deserializer<'de>,
     {
-        let [p, q] = <[Integer; 2]>::deserialize(deserializer)?;
-        DecryptionKey::from_primes(p, q)
-            .map_err(|_| <D::Error as serde::de::Error>::custom("invalid paillier key"))
+        let [n_size, a_size] = <[u32; 2]>::deserialize(deserializer)?;
+        DecryptionKey::generate(n_size, a_size)
+            .map_err(|_| <D::Error as serde::de::Error>::custom("invalid paillier decryption key"))
     }
 }
